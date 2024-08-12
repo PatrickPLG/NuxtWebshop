9e7e62587b02b079a3e37ddef2759c1a0256ee36
Patrick Gravengaard <p-l-g@hotmail.dk>
2024-08-12 15:18:47 +0200

    Added SEO data

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+useSeoMeta({
+  title: 'Nuxt Webshop',
+  description: 'Shop all our needs',
+})
 
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.ico'
+    }
+  ]
+})
 </script>
 
 <template>
